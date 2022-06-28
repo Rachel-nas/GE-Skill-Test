54bdb80aec64cd001b33e7fc7cb5a5ce499e8b82
Rachel Nascimento <rachelle.nascimento@gmail.com>
2022-06-28 10:51:27 +0200

    add root to and corrections in the redirections.yml file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'pages#show'
   get '*path', to: 'pages#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
